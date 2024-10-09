23023f9c33e5a2e8d1467ef60aeaf245a10d12e6
Shrikanth K <shreekikoppad@gmail.com>
2024-10-09 17:42:32 +0530

    added the code for error detection for command line

--- basic_calculator.c
+++ basic_calculator.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(int argc,char ** argv)
+{
+        if(argc!=4)
+        {
+            printf("./a.out num1  operator  num2\n");
+            return 0;
+        }
+}
