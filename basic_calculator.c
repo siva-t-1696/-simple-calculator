8998cdec9ca442075ea14b3ce07531fb63a40c56
ramesh <rameshkundru1@gmail.com>
2024-10-09 18:36:25 +0530

    Codedded for div and mul

--- basic_calculator.c
+++ basic_calculator.c
@@ -1,1 +1,17 @@
-
+void mul(int a ,int b)
+{
+    printf("The two numbers are a=%d b=%d\n",a,b);
+    print("multiplication of two numbers result is a*b=\n",a*b);
+}
+void div(int a,int b)
+{
+    if(b==0)
+    {
+        printf("you will get floating point exception\n");
+        return;
+    }
+    else
+    {
+        printf("division of two numbers is :%d\n",a/b);
+    }
+}
